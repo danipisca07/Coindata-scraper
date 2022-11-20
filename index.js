c29eb410d3459fc7a88f0b4500acd0f20477e753
danipisca07 <danielepiscaglia@libero.it>
2022-11-20 11:06:04 +0100

    Get top 10 coins

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
 
-const coinList = [ "ark" ]
-const baseUrl = "https://coinmarketcap.com/"
-const currenciesPath = "currencies/"
+const baseUrl = "https://coinmarketcap.com"
 
 async function main() {
+    const coinList = await getTopCoins(10)
+    await getWatchlists(coinList)
+}
+
+async function getTopCoins() {
+    const html = await getHtml(baseUrl)
+    const $= cheerio.load(html);
+    const coinList = []
+    const coinItems= $(".eVOXbZ .LCOyB a");
+    coinItems.each((index, data) => {
+        coinList.push($(data).attr('href'))
+    })
+    return coinList
+}
+
+async function getWatchlists(coinList){
     for(let i = 0; i < coinList.length; i++) {
-        const coin = coinList[i]
-        const html = await getHtml(baseUrl + currenciesPath + coin)
+        const coinPath = coinList[i]
+        const html = await getHtml(baseUrl + coinPath)
         const $= cheerio.load(html);
 
         const datarow= $(".cevGxl > .namePill:last-of-type");
-        datarow.each((i, data) => {
+        datarow.each((index, data) => {
             const item= $(data).text();
             const num = new Number(item.replace(/\D/g,''));
-            console.log(`${coin} on ${num} watchlists`)
+            console.log(`${coinPath} on ${num} watchlists`)
         })
     }
 }
