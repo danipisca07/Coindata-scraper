4ce5f78bf24e09226b6b071b02830d8c9fb959e7
danipisca07 <danielepiscaglia@libero.it>
2022-11-23 00:27:21 +0100

    Comparing with previous value

--- index.js
+++ index.js
@@ -7,18 +7,21 @@ const MongoClient = require('mongodb').MongoClient;
 const baseUrl = "https://coinmarketcap.com"
 const coinDataCollection = "CoinData"
 
-const TOP_N_COINS = 1
+const TOP_N_COINS = 5
 
 async function main() {
     console.log("Retrieving top coins...\n")
     const coinList = await getTopCoins(TOP_N_COINS)
-    console.log("Top " + TOP_N_COINS + " retrieved.")
+    console.log("Top " + TOP_N_COINS + " retrieved.\nDone\n-------")
     console.log("Retrieving coins data...\n")
     const res = await getCoinInfo(coinList)
-    console.log("Done")
+    console.log("Done\n-------")
     console.log("Storing on mongoDb...")
     await storeCoinData(res)
-    console.log("Done")
+    console.log("Done\n-------")
+    console.log("Comparing with last entry...")
+    await compareWithPreviousEntry(res)
+    console.log("Done\n-------")
     process.exit()
 }
 
@@ -112,6 +115,62 @@ async function storeCoinData(coinData){
     }
 }
 
+async function compareWithPreviousEntry(coinData){
+    try {
+        const db = await MongoClient.connect(process.env.MONGO_CONNSTRING)
+
+        const aggregation = [
+            //FILTRO
+              {
+                '$match': {
+                  'partitionDate': { '$lt': partitionDate() },
+                  'coin': { $in: coinData.map(x => x.coin) }
+                }
+              }, 
+            //ORDINAMENTO
+            {
+                '$sort': {
+                  'partitionDate': -1
+                }
+              }, 
+            //GROUPING CON SELEZIONE INTERO ELEMENTO
+            {
+                '$group': {
+                  '_id': '$coin', 
+                  'lastDocument': {
+                    '$first': '$$CURRENT'
+                  }
+                }
+              },
+            //REWRITE DELLA ROOT PER ESPORRE DOCUMENTO AS ORIGINAL
+            {
+                '$replaceRoot': {
+                  'newRoot': '$lastDocument'
+                }
+            }
+        ];
+        const previousValues = await db.db(process.env.MONGO_DB).collection(coinDataCollection).aggregate(aggregation).toArray();
+        
+        var tuples = coinData.map(x => {
+            return { newValue: x, oldValue: previousValues.find(y => y.coin === x.coin) }
+        })
+
+        var newEntries = tuples.filter(x => x.oldValue === undefined)
+        var improvements = tuples.filter(x => x.oldValue !== undefined).map(x => {
+            const diffTime = Math.abs(x.newValue.date - x.oldValue.date);
+            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+            return {
+                coin: x.newValue.coin,
+                watchlist: (x.newValue.watchlist - x.oldValue.watchlist) / diffDays
+            }
+        })
+        improvements.forEach(x => console.log(x.coin + " - WatchList: " + x.watchlist + "/day"));
+    } catch(e)
+    {
+        console.error(e)
+    }
+}
+
 async function dumpToFile(data, dir, file){
     const dump = data //JSON.stringify(data, null, "\t")
     return new Promise((resolve, reject) => {
@@ -131,7 +190,7 @@ function partitionDate(date = undefined) {
     if (day.length < 2) 
         day = '0' + day;
 
-    return [year, month, day].join('');
+    return parseInt([year, month, day].join(''));
 }
 
 async function getHtml(url){
